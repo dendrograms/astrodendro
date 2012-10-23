8fa615d6a8a02f5cc00a312e519f7b07ad56c153
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-10-23 16:03:17 +0200

    Add built-in py.test tester

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, Command
 
 try:  # Python 3.x
     from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:  # Python 2.x
     from distutils.command.build_py import build_py
+
+
+class DendroTest(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
 
 setup(name='dendro-core',
       version='0.0.1',
@@ -15,7 +32,7 @@ setup(name='dendro-core',
       packages=['astrodendro', 'astrodendro.io', 'astrodendro.test'],
       provides=['astrodendro'],
       requires=['numpy'],
-      cmdclass={'build_py': build_py},
+      cmdclass={'build_py': build_py, 'test': DendroTest},
       keywords=['Scientific/Engineering'],
       classifiers=[
                    "Development Status :: 4 - Beta",
