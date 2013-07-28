6036015361dbb8fb2d321fa408763c9633b3d44b
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-07-28 20:15:40 +0200

    Fixed behavior and added more tests

--- astrodendro/io/__init__.py
+++ astrodendro/io/__init__.py
@@ -18,18 +18,24 @@ IO_FORMATS = {
 
 
 def load_dendrogram(filename, format=None):
-    for io_format in IO_FORMATS:
-        if IO_FORMATS[io_format]['identify'](filename, mode='r'):
-            return IO_FORMATS[io_format]['import_dendro'](filename)
+    if format is not None:
+        return IO_FORMATS[format]['import_dendro'](filename)
+    else:
+        for io_format in IO_FORMATS:
+            if IO_FORMATS[io_format]['identify'](filename, mode='r'):
+                return IO_FORMATS[io_format]['import_dendro'](filename)
     raise IOError("Could not automatically identify file format - use the "
                   "format= option to specify which format to use (valid "
                   "options are 'fits' and 'hdf5')")
 
 
 def save_dendrogram(dendrogram, filename, format=None):
-    for io_format in IO_FORMATS:
-        if IO_FORMATS[io_format]['identify'](filename, mode='w'):
-            return IO_FORMATS[io_format]['export_dendro'](dendrogram, filename)
+    if format is not None:
+        return IO_FORMATS[format]['export_dendro'](dendrogram, filename)
+    else:
+        for io_format in IO_FORMATS:
+            if IO_FORMATS[io_format]['identify'](filename, mode='r'):
+                return IO_FORMATS[io_format]['export_dendro'](dendrogram, filename)
     raise IOError("Could not automatically identify file format - use the "
                   "format= option to specify which format to use (valid "
                   "options are 'fits' and 'hdf5')")
