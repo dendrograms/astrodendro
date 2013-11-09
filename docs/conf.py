00149408e4d1ad6a5da1eb26b34cfb31b289e757
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-11-09 15:36:52 +0100

    Fix any instances of AstroDendro to astrodendro for consistency

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# AstroDendro documentation build configuration file, created by
+# astrodendro documentation build configuration file, created by
 # sphinx-quickstart on Thu Jun 13 14:25:14 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -79,7 +79,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'AstroDendro'
+project = u'astrodendro'
 copyright = u'2013, Thomas Robitaille, Chris Beaumont, Braden McDonald, and Erik Rosolowsky'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -204,7 +204,7 @@ html_static_path = []
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'AstroDendrodoc'
+htmlhelp_basename = 'astrodendrodoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -223,7 +223,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'AstroDendro.tex', u'AstroDendro Documentation',
+  ('index', 'astrodendro.tex', u'astrodendro Documentation',
    u'Thomas Robitaille, Braden McDonald, Chris Beaumont, Erik Rosolowsky', 'manual'),
 ]
 
@@ -253,7 +253,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'astrodendro', u'AstroDendro Documentation',
+    ('index', 'astrodendro', u'astrodendro Documentation',
      [u'Thomas Robitaille, Braden McDonald, Chris Beaumont, Erik Rosolowsky'], 1)
 ]
 
@@ -267,8 +267,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'AstroDendro', u'AstroDendro Documentation',
-   u'Thomas Robitaille, Braden McDonald, Chris Beaumont, Erik Rosolowsky', 'AstroDendro', 'One line description of project.',
+  ('index', 'astrodendro', u'astrodendro Documentation',
+   u'Thomas Robitaille, Braden McDonald, Chris Beaumont, Erik Rosolowsky', 'astrodendro', 'One line description of project.',
    'Miscellaneous'),
 ]
 
