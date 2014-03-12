b69444f110f14fd1a11cafb7c3132009b262fd96
Tom Rice <t.rice90@gmail.com>
2014-03-12 12:12:30 -0400

    Implemented my half of the deal, partly.

--- astrodendro/dendro_scatter.py
+++ astrodendro/dendro_scatter.py
@@ -1,24 +1,44 @@
-
-
 import matplotlib.pyplot as plt
 
 class Selection(object):
-    selected = {} # selection id -> list of dendrogram structure ids
-    colors = {}  # selection id -> a color
+    def __init__(self):
+        self.selected = {} # selection id -> list of dendrogram structure ids
+        self.colors = {}  # selection id -> a color
 
 
 class DendroScatter(object):
 
     def __init__(self, dendrogram, catalog, xaxis, yaxis):
         self.fig = plt.figure()
-        pass
+        self.axes = plt.subplot(1,1,1)
+        self.catalog = catalog
+        self.xdata = catalog[xaxis]
+        self.ydata = catalog[yaxis]
+
+        self.x_column_name = xaxis
+        self.y_column_name = yaxis
 
     def _draw_plot(self):
-        pass
+
+        self.axes.plot(self.xdata, self.ydata, 'o', color='w', mec='k')
+
+        self.axes.set_xlabel(self.x_column_name)
+        self.axes.set_ylabel(self.y_column_name)
+
+        self.fig.canvas.draw()
 
     def update_selection(self, selection):
         """Highlight seleted structures"""
-        pass
+        
+        self.fig.cla()
+        self._draw_plot()
+
+        for key in selection.selected.keys():
+            self.axes.plot(
+                self.xdata[selection.selected[key]], 
+                self.ydata[selection.selected[key]], 
+                'o', color=selection.colors[key])
+
 
     def select(self, structure, index):
         "Select a given structure and assign it to index-th selection"
