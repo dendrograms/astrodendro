ee9e68e35232a72c64e591e005e6ff567635865e
Tom Rice <t.rice90@gmail.com>
2014-06-09 02:00:25 -0400

    Updated the Kelvin flux test case to reflect the new (correct) signature.

--- astrodendro/tests/test_flux.py
+++ astrodendro/tests/test_flux.py
@@ -17,7 +17,7 @@ COMBINATIONS = \
 (np.array([1, 2, 3]) * u.erg / u.cm ** 2 / u.s / u.micron, u.Jy, {'wavelength': 2 * u.micron}, 8005538284.75565 * u.Jy),
 (np.array([1, 2, 3]) * u.Jy / u.arcsec ** 2, u.Jy, {'spatial_scale': 2 * u.arcsec}, 24. * u.Jy),
 (np.array([1, 2, 3]) * u.Jy / u.beam, u.Jy, {'spatial_scale': 2 * u.arcsec, 'beam_major': 1 * u.arcsec, 'beam_minor': 0.5 * u.arcsec}, 42.36166269526079 * u.Jy),
-(np.array([1, 2, 3]) * u.K, u.Jy, {'beam_major': 1 * u.arcsec, 'beam_minor': 0.5 * u.arcsec, 'wavelength': 2 * u.mm}, 0.03823086231417533 * u.Jy),
+(np.array([1, 2, 3]) * u.K, u.Jy, {'spatial_scale': 2 * u.arcsec, 'beam_major': 1 * u.arcsec, 'beam_minor': 0.5 * u.arcsec, 'wavelength': 2 * u.mm}, 0.26992048231700877 * u.Jy),
     ]
 
 
