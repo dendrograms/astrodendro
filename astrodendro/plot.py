dendrograms/astrodendro

commit 4c5a6432be45b9878b1df84ba38aa4186ca6712b

Implemented non-subtree selections, so that Lasso select can work.
Fairly substantial change -- I've tested it decently well and I think it works great!
For most situations the syntax doesn't change but in some cases it does.

I think the only thing left "dangling" is that contour selection still only uses the "zeroth" struct, so I gotta use a loop in that case.

astrodendro/plot.py
@@ -158,14 +158,17 @@ class DendrogramPlotter(object):
 
         ax.contour(mask, levels=[0.5], **kwargs)
 
-    def get_lines(self, structure=None, **kwargs):
+    def get_lines(self, structures=None, subtree=True, **kwargs):
         """
         Get a collection of lines to draw the dendrogram.
 
         Parameters
         ----------
-        structure : :class:`~astrodendro.structure.Structure`
-            The structure to plot. If not set, the whole tree will be plotted.
+        structures : :class:`~astrodendro.structure.Structure`
+            The structures to plot. If not set, the whole tree will be plotted.
+        subtree : bool, optional
+            If a structure is specified, by default the whole subtree will be
+            retrieved, but this can be disabled with this option.
 
         Returns
         -------
@@ -181,12 +184,18 @@ class DendrogramPlotter(object):
         if self._cached_positions is None:
             raise Exception("Leaves have not yet been sorted")
 
-        if structure is None:
+        # Case 1: no structures are selected
+        if structures is None:
             structures = list(self.dendrogram.all_structures)
+        # Case 2: one structure is selected, and subtree is True
         else:
-            if type(structure) is int:
-                structure = self.dendrogram[structure]
-            structures = structure.descendants + [structure]
+            if subtree:
+                if type(structures[0]) is int:
+                    structure = self.dendrogram[structures[0]]
+                else: 
+                    structure = structures[0]
+                structures = structure.descendants + [structure]
+        # Case 3: subtree is False (do nothing special to `structures`)
 
         lines = []
         mapping = []

astrodendro/scatter.py
@@ -57,9 +57,7 @@ class Scatter(object):
 
             selected_structures = [structure for structure in self.structures if structure.idx in indices]
 
-            # This doesn't technically work as desired yet, because you'll end up implicitly selecting all of this structure's children.
-            # Also if you lasso more than one point you'll hit a NotImplementedError in BasicDendrogramViewer.
-            self.hub.select(input_key, selected_structures)
+            self.hub.select(input_key, selected_structures, subtree=False)
 
             self.fig.canvas.draw_idle()
             self.fig.canvas.widgetlock.release(self.lasso)
@@ -82,11 +80,16 @@ class Scatter(object):
                 self.lines2d[selection_id].remove()
                 del self.lines2d[selection_id]
 
-        struct = self.hub.selections[selection_id][0]
+        structures = self.hub.selections[selection_id]
+        struct = structures[0]
+
         if struct is None:
             self.fig.canvas.draw()
             return
-        selected_indices = [leaf.idx for leaf in struct.descendants + [struct]]
+        if self.hub.select_subtree[selection_id]:
+            selected_indices = [leaf.idx for leaf in struct.descendants + [struct]]
+        else:
+            selected_indices = [leaf.idx for leaf in structures]
 
         self.lines2d[selection_id] = self.axes.plot(
             self.xdata[selected_indices], 

astrodendro/viewer.py
@@ -17,6 +17,7 @@ class SelectionHub(object):
     def __init__(self):
         # map selection IDs -> list of selected dendrogram IDs
         self.selections = {}
+        self.select_subtree = {} # map selection IDs -> bool
         self.colors = defaultdict(lambda: 'red')
         self.colors[1] = '#e41a1c' 
         self.colors[2] = '#377eb8'
@@ -35,9 +36,9 @@ class SelectionHub(object):
         """
         self._callbacks.append(method)
 
-    def select(self, id, structure):
+    def select(self, id, structures, subtree=True):
         """
-        Select a new structure, and associate
+        Select new structures, and associate
         them with a selection ID
 
         Parameters
@@ -45,10 +46,11 @@ class SelectionHub(object):
         id : int
         structures : list of Dendrogram Structures to select
         """
-        if not isinstance(structure, list):
-            structure = [structure]
+        if not isinstance(structures, list):
+            structures = [structures]
 
-        self.selections[id] = structure
+        self.selections[id] = structures
+        self.select_subtree[id] = subtree
         for cb in self._callbacks:
             cb(id)
 
@@ -257,11 +259,10 @@ class BasicDendrogramViewer(object):
         event.canvas.draw()
 
     def _update_lines(self, selection_id):
-        structure = self.hub.selections[selection_id]
-        if len(structure) > 1:
-            raise NotImplementedError(
-                "Multiple structures per selection not supported")
-        structure = structure[0]
+        structures = self.hub.selections[selection_id]
+        select_subtree = self.hub.select_subtree[selection_id]
+
+        structure = structures[0]
 
         # Remove previously selected collection
         if selection_id in self.selected_lines:
@@ -276,12 +277,18 @@ class BasicDendrogramViewer(object):
 
         self.remove_all_contours()
 
-        self.selected_label[selection_id].set_text(
-            "Selected structure: {0}".format(structure.idx))
+        if len(structures) <= 1:
+            label_text = "Selected structure: {0}".format(structure.idx)
+        elif len(structures) <=3:
+            label_text = "Selected structures: {0}".format(', '.join([str(structure.idx) for structure in structures]))
+        else:
+            label_text = "Selected structures: {0}...".format(', '.join([str(structure.idx) for structure in structures[:3]]))
+
+        self.selected_label[selection_id].set_text(label_text)
 
         # Get collection for this substructure
         self.selected_lines[selection_id] = self.plotter.get_lines(
-            structure=structure)
+            structures=structures, subtree=select_subtree)
         self.selected_lines[selection_id].set_color(self.hub.colors[selection_id])
         self.selected_lines[selection_id].set_linewidth(2)
         self.selected_lines[selection_id].set_zorder(structure.height)
@@ -306,9 +313,7 @@ class BasicDendrogramViewer(object):
 
         for selection_id in self.hub.selections.keys():
             struct = self.hub.selections[selection_id]
-            if len(struct) != 1:
-                raise NotImplementedError(
-                    "Multiple structures per selection not supported")
+
             struct = struct[0]
             if struct is None:
                 continue

commit ea44b248faa78f3186b77a2655271987490fca0f

fix bug introduced in 4c5a6432be45b9878b1df84ba38aa4186ca6712b: 'structures' can be an int (since plot_tree accepts singular 'structure' which intuitively should be an int)

astrodendro/plot.py
@@ -190,7 +190,9 @@ class DendrogramPlotter(object):
         # Case 2: one structure is selected, and subtree is True
         else:
             if subtree:
-                if type(structures[0]) is int:
+                if type(structures) is int:
+                    structure = self.dendrogram[structures]
+                elif type(structures[0]) is int:
                     structure = self.dendrogram[structures[0]]
                 else: 
                     structure = structures[0]