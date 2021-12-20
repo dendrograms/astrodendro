90a5e8fc5b9f654f9d699dd708e8bdf1f379883b
Remy Indebetouw <remy@Pika.local>
2021-12-20 09:38:07 -0500

    h5py version >=3 has deprecated Structure.value this replaces the .value with conversions to string and numpy array as appropriate

--- astrodendro/io/hdf5.py
+++ astrodendro/io/hdf5.py
@@ -59,9 +59,14 @@ def dendro_import_hdf5(filename):
 
     log.debug('Loading HDF5 file from disk...')
     with h5py.File(filename, 'r') as h5f:
-        newick = h5f['newick'].value
-        data = h5f['data'].value
-        index_map = h5f['index_map'].value
+        if h5py.__version__>'3':
+            newick = h5f['newick'][()].decode("utf-8") # str
+            data = h5f['data'][:] # numpy array
+            index_map = h5f['index_map'][:] # numpy array
+        else:
+            newick = h5f['newick'].value
+            data = h5f['data'].value
+            index_map = h5f['index_map'].value
 
         params = {}
         if 'min_value' in h5f.attrs:
@@ -70,7 +75,7 @@ def dendro_import_hdf5(filename):
             params['min_npix'] = h5f.attrs['min_npix']
 
         try:
-            wcs = WCS(h5f['wcs_header'].value)
+            wcs = WCS(h5f['wcs_header'][()])
         except KeyError:
             wcs = None
 
