8f9aa77c1be1667cbc5a38e724353ceff2009244
Thomas Robitaille <thomas.robitaille@gmail.com>
2022-07-20 12:02:52 +0100

    Fix imports

--- astrodendro/__init__.py
+++ astrodendro/__init__.py
@@ -1,9 +1,9 @@
 # Licensed under an MIT open source license - see LICENSE
 
-from astrodendro.dendrogram import Dendrogram, periodic_neighbours  # noqa
-from astrodendro.structure import Structure  # noqa
-from astrodendro.analysis import ppv_catalog, pp_catalog  # noqa
-from astrodendro.plot import DendrogramPlotter  # noqa
-from astrodendro.viewer import BasicDendrogramViewer  # noqa
+from .dendrogram import Dendrogram, periodic_neighbours  # noqa
+from .structure import Structure  # noqa
+from .analysis import ppv_catalog, pp_catalog  # noqa
+from .plot import DendrogramPlotter  # noqa
+from .viewer import BasicDendrogramViewer  # noqa
 
-from astrodendro.version import version as __version__  # noqa
+from .version import version as __version__  # noqa
